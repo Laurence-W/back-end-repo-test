3c9bd0a137f421abe80779f9e2e5eea541d0f175
Laurence-W <laurenceother@hotmail.com>
2023-07-19 13:57:13 +1000

    changing express port

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ dotenv.config();
 const express = require("express");
 const app = express();
 const HOST = process.env.HOST || "localhost";
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const helmet = require("helmet");
 app.use(helmet());
