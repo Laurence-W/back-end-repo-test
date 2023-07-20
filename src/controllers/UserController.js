22cd42082f22bd037fbf862cf1e7533e832bcc8d
Liam-M-Dev <liammassey21@gmail.com>
2023-07-20 17:35:27 +1000

    Scaffold conditional checks for createUser function

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -6,6 +6,22 @@ const {User} = require("../models/UserModel");
 
 
 // Sign up function 
-// const createUser = async (request, response) => {
-    
-// }
+const createUser = async (request, response) => {
+    let {firstName, lastName, username, email, password} = request.body;
+
+    // Conditional check to ensure all fields have data
+    if (!firstName || !lastName || !username || !email || !password) {
+        return response.status(400).json({message: "Please enter in all fields"})
+    }
+
+    // Conditional check for password length
+    if (password.length < 8) {
+        return response.status(400).json({message: "Password not long enough, please enter 8 or more characters"});
+    }
+
+    // Conditional check to see if user exists within db
+    let savedUser = await User.findOne({email: email});
+    if (savedUser) {
+        return response.status(422).json({message: "User already exists with email"});
+    }
+}
