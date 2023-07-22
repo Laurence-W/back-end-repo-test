f5a4fdf2f1832ab5d84b2a6ab7a43946e815cb39
Liam-M-Dev <liammassey21@gmail.com>
2023-07-22 12:56:07 +1000

    Add password length middlware validator

--- src/middleware/UserMiddleware.js
+++ src/middleware/UserMiddleware.js
@@ -2,8 +2,10 @@ const {User} = require("../models/UserModel");
 
 // Checks all user fields have been entered for sign up and updating user
 const checkUserFields = (request, response, next) => {
+    // Destructure Request Object into variables
     let {firstName, lastName, username, email, password} = request.body;
     
+    // Conditional to ensure each variable has data
     if (!firstName || !lastName || !username || !email || !password) {
         return response.status(400).json({message: "Please enter in all fields"});
     } else {
@@ -13,7 +15,9 @@ const checkUserFields = (request, response, next) => {
 
 // Checks inputted email is not being used by another account
 const checkValidEmail = async (request, response, next) => {
+    // Search database for user with related email from request body
     let savedUser = await User.findOne({email: request.body.email});
+    // Conditional to check if a user is found within the database, return error
     if (savedUser) {
         return response.status(422).json({message: "User already exists with email"});
     } else {
@@ -21,7 +25,17 @@ const checkValidEmail = async (request, response, next) => {
     }
 }
 
+// Password Validation ensuring password length is above 8 characters
+const checkPasswordLength = (request, response, next) => {
+    if (request.body.password.length < 8) {
+        return response.status(400).json({message: "Password length is too short, enter 8 or more characters"});
+    } else {
+        next();
+    }
+}
+
 module.exports = {  
     checkUserFields,
-    checkValidEmail
+    checkValidEmail,
+    checkPasswordLength
 };
--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 // Import functions from userControllers here
 const {createUser} = require("../controllers/UserController");
-const { checkUserFields, checkValidEmail } = require("../middleware/UserMiddleware");
+// Middleware Imports
+const { checkUserFields, checkValidEmail, checkPasswordLength } = require("../middleware/UserMiddleware");
 
-router.post("/signup", checkUserFields, checkValidEmail, createUser);
+router.post("/signup", checkUserFields, checkValidEmail, checkPasswordLength, createUser);
 
 
 module.exports = router;
